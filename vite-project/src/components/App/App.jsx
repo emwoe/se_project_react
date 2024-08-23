4fcb685475a449ff63e2de4516918fac36c028c0
Emma Woelk <emma.woelk@gmail.com>
2024-08-23 14:08:36 -0500

    feat item card and filters

--- vite-project/src/components/App/App.jsx
+++ vite-project/src/components/App/App.jsx
@@ -1,13 +1,16 @@
+import { useState } from "react";
+
 import "./App.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 
 function App() {
+  const [weatherData, setWeatherData] = useState({ type: "hot" });
   return (
     <div className="app">
       <div className="app__content">
         <Header />
-        <Main />
+        <Main weatherData={weatherData} />
       </div>
     </div>
   );
--- vite-project/src/components/Main/ItemCard/ItemCard.jsx
+++ vite-project/src/components/Main/ItemCard/ItemCard.jsx
@@ -1,5 +1,12 @@
-function ItemCard() {
-  return <></>;
+import "./ItemCard.css";
+
+function ItemCard({ item }) {
+  return (
+    <div>
+      <h2>{item.name}</h2>
+      <img src={item.link} alt={item.name}></img>
+    </div>
+  );
 }
 
 export default ItemCard;
